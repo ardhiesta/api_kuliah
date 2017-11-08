788943fe7f41baed747057f5f7aa660aa288d5cd
ardhiesta <ardhi.mail@gmail.com>
2017-11-09 04:50:32 +0700

    first commit

---
+++ classes/MahasiswaMapper.php
@@ -0,0 +1,70 @@
+<?php
+
+class MahasiswaMapper extends Mapper
+{
+    public function getAllMhs() {
+        $sql = "SELECT * from tbl_mahasiswa";
+        $stmt = $this->db->query($sql);
+        
+        $results = $stmt->fetchAll();
+        
+        return $results;
+    }
+
+    public function getMahasiswaByNim($nim) {
+        $sql = "SELECT * from tbl_mahasiswa where nim = :nim";
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute(["nim" => $nim]);
+
+        if($result) {
+            return $stmt->fetch();
+        }
+
+    }
+
+    public function saveMahasiswa($nim, $nama, $alamat) {
+        $sql = "insert into tbl_mahasiswa
+            (nim, nama, alamat) values
+            (:nim, :nama, :alamat)";
+
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute([
+            "nim" => $nim,
+            "nama" => $nama,
+            "alamat" => $alamat,
+        ]);
+
+        if(!$result) {
+            throw new Exception("could not save record");
+        }
+    }
+    
+    public function updateMahasiswa($old_nim, $nim, $nama, $alamat) {
+        $sql = "update tbl_mahasiswa set nim = :nim, nama = :nama, alamat = :alamat where nim = :old_nim";
+
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute([
+            "old_nim" => $old_nim,
+            "nim" => $nim,
+            "nama" => $nama,
+            "alamat" => $alamat,
+        ]);
+
+        if(!$result) {
+            throw new Exception("could not update record");
+        }
+    }
+    
+    public function deleteMahasiswa($nim) {
+		$sql = "delete from tbl_mahasiswa where nim = :nim";
+		
+		$stmt = $this->db->prepare($sql);
+        $result = $stmt->execute([
+            "nim" => $nim
+        ]);
+
+        if(!$result) {
+            throw new Exception("could not delete record");
+        }
+	}
+}
